027de27af9ae1895bc766df3014fe3e013273f30
Parshav Shah <parshavshah1995@gmail.com>
2025-05-09 11:15:47 +0530

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -20,62 +20,92 @@ router.get("/home", commonFunction);
 
 // login view
 router.get("/login", (req, res, next) => {
-  res.render("login", { BASE_URL: process.env.BASE_URL });
+  res.render("login", { user: req.session.user, BASE_URL: process.env.BASE_URL });
 });
 
 // register view
 router.get("/register", (req, res, next) => {
-  res.render("register", { BASE_URL: process.env.BASE_URL });
+  res.render("register", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 // dashboard view
 router.get("/dashboard", isAuthenticated, (req, res, next) => {
-  res.render("dashboard", { BASE_URL: process.env.BASE_URL });
+  res.render("dashboard", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 // firms view
 router.get("/firms", isAuthenticated, (req, res, next) => {
-  res.render("firms", { BASE_URL: process.env.BASE_URL });
+  res.render("firms", { user: req.session.user, BASE_URL: process.env.BASE_URL });
 });
 
 // clients view
 router.get("/clients", isAuthenticated, (req, res, next) => {
-  res.render("clients", { BASE_URL: process.env.BASE_URL });
+  res.render("clients", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 // products view
 router.get("/products", isAuthenticated, (req, res, next) => {
-  res.render("products", { BASE_URL: process.env.BASE_URL });
+  res.render("products", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 // taxes view
 router.get("/taxes", isAuthenticated, (req, res, next) => {
-  res.render("taxes", { BASE_URL: process.env.BASE_URL });
+  res.render("taxes", { user: req.session.user, BASE_URL: process.env.BASE_URL });
 });
 
 router.get("/invoice/list", isAuthenticated, (req, res, next) => {
-  res.render("invoice/list", { BASE_URL: process.env.BASE_URL });
+  res.render("invoice/list", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 router.get("/invoice/create", isAuthenticated, (req, res, next) => {
-  res.render("invoice/create", { BASE_URL: process.env.BASE_URL });
+  res.render("invoice/create", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 router.get("/invoice/update/:id", isAuthenticated, (req, res, next) => {
-  res.render("invoice/update", { BASE_URL: process.env.BASE_URL });
+  res.render("invoice/update", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 // user profile view
 router.get("/profile", isAuthenticated, (req, res, next) => {
-  res.render("profile", { BASE_URL: process.env.BASE_URL });
+  res.render("profile", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 // change password view
 router.get("/change-password", isAuthenticated, (req, res, next) => {
-  res.render("change-password", { BASE_URL: process.env.BASE_URL });
+  res.render("change-password", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 // forgot password view
 router.get("/forgot-password", isAuthenticated, (req, res, next) => {
-  res.render("dashboard", { BASE_URL: process.env.BASE_URL });
+  res.render("dashboard", {
+    user: req.session.user,
+    BASE_URL: process.env.BASE_URL,
+  });
 });
 
 module.exports = router;
