243558e59d1c986f2255c2c2ab0fe5a0f745f5c5
Parshav Shah <parshavshah1995@gmail.com>
2025-06-07 14:56:49 +0530

    Completed payment work

---
+++ demo/13_invoicemate_table_payment_methods.sql
@@ -0,0 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 07, 2025 at 11:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+
+INSERT INTO `payment_methods` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
+(8, 'Cash', 'Payment received by cash', '2025-06-07 09:04:38', '2025-06-07 09:04:38');
---
+++ demo/14_invoicemate_table_payments.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO `payments` (`id`, `invoice_id`, `payment_method_id`, `amount`, `payment_date`, `reference_number`, `transaction_id`, `notes`, `status`, `created_by`, `created_at`, `updated_at`) VALUES
+(8, 4, 8, 1000.00, '2025-06-03', 'PAT002432', 'PAT002432', 'Client sent payment by cash today', 'completed', 1, '2025-06-07 09:05:36', '2025-06-07 09:05:36'),
+(9, 1, 8, 2000.00, '2025-06-04', 'PAT002434', 'PAT002434', 'Cash Payment Received', 'completed', 1, '2025-06-07 09:06:07', '2025-06-07 09:06:07'),
+(10, 5, 8, 18453.79, '2025-06-05', '', '', 'Complete Cash Payment Received', 'completed', 1, '2025-06-07 09:06:38', '2025-06-07 09:06:38'),
+(11, 3, 8, 6000.00, '2025-06-06', '', '', '6000 Cash Recived', 'completed', 1, '2025-06-07 09:06:58', '2025-06-07 09:06:58');
